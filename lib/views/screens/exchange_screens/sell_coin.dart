6efa51ff92d5446ec527d23bbeaf4d6fb48ea2b0
cephas <cephasarowolo@gmail.com>
2021-12-26 08:57:30 +0100

    Started Sell Page

--- lib/views/screens/exchange_screens/sell_coin.dart
+++ lib/views/screens/exchange_screens/sell_coin.dart
@@ -1,11 +1,16 @@
 
+import 'dart:convert';
+import '../../../style_sheet.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pay_buy_max/controllers/providers/coin_price_provider.dart';
 import 'package:pay_buy_max/controllers/providers/user_provider.dart';
+import 'package:pay_buy_max/models/auth/sign_in_response_entity.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:pay_buy_max/models/wallet/wallet_balance_entity.dart';
-
 
 class SellCoin extends StatelessWidget {
   const SellCoin();
@@ -34,6 +39,8 @@ class SellCoinWidget extends StatefulWidget {
 
 class _SellCoinWidgetState extends State<SellCoinWidget> {
   WalletBalanceEntity? walletBalanceEntity;
+  late SignInResponseEntity args;
+
   late TextEditingController textController;
   final scaffoldKey = GlobalKey<ScaffoldState>();
   String value = "Bitcoin";
@@ -43,18 +50,158 @@ class _SellCoinWidgetState extends State<SellCoinWidget> {
   void initState() {
     super.initState();
     textController = TextEditingController();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _getWalletInfo();
+    });
+  }
+
+  void _getWalletInfo(){
+    // final response = await getWalletInfo();
+    getWalletInfo().then((value){
+      setState(() {
+        if(value.status == true){
+          walletBalanceEntity = value;
+        }else{
+          walletBalanceEntity = null;
+          /*if(response.message == null){
+        AppOverlay.snackbar(message: "An Error Occurred!. Please Try Again");
+      }else{
+        if(response.message!.isEmpty){
+          AppOverlay.snackbar(message: "An Error Occurred!. Please Try Again");
+        }else{
+          AppOverlay.snackbar(message: response.message.toString());
+        }
+      }*/
+        }
+      });
+    });
+  }
+
+  Future<WalletBalanceEntity> getWalletInfo() async{
+    String url = 'https://paybuymax.com/api/user';
+    final response = await http.get(Uri.parse(url),headers: {"Authorization":args.token.toString()});
+    return WalletBalanceEntity().fromJson(json.decode(response.body));
   }
 
   @override
   Widget build(BuildContext context) {
+    args = ModalRoute.of(context)!.settings.arguments as SignInResponseEntity;
+
     return Scaffold(
       key: scaffoldKey,
-      backgroundColor: Color(0xFFF5F5F5),
+      backgroundColor: StyleSheet.primaryColor.withOpacity(0.09),
       body: SafeArea(
         child: Expanded(
           child: ListView(
             scrollDirection: Axis.vertical,
             children: [
+              Builder(
+                builder: (context) {
+                  if(walletBalanceEntity == null){
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: CircularProgressIndicator(
+                          color: Color(0xFFC9782F),
+                        ),
+                      ),
+                    );
+                  } else{
+                    if(walletBalanceEntity!.user!.userCoins == null){
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: CircularProgressIndicator(
+                            color: Color(0xFFC9782F),
+                          ),
+                        ),
+                      );
+                    }else if(walletBalanceEntity!.user!.userCoins!.isEmpty){
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: CircularProgressIndicator(
+                            color: Color(0xFFC9782F),
+                          ),
+                        ),
+                      );
+                    }
+                  }
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, position) {
+                      var coin = walletBalanceEntity!.user!.userCoins!.elementAt(position);
+                      int? rate = coin.coinTypes!.dollarRate;
+                      String rateText = "USD "+rate!.toString();
+
+                      return Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Color(0xFFF5F5F5),
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 8),
+                                child: CircleAvatar(
+                                    backgroundColor: Colors.transparent,
+                                    child: CachedNetworkImage(
+                                      imageUrl: coin.coinTypes!.image.toString(),
+                                      placeholder: (context, url) => new CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) => new Icon(Icons.error),
+                                    )),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(coin.coinTypes!.name.toString(), style: TextStyle(
+                                        color: Colors.black, fontSize: 18),
+                                        textAlign: TextAlign.start),
+                                    Text(coin.coinTypes!.symbol.toString().toUpperCase(), style: TextStyle(
+                                        color: Colors.black54, fontSize: 18),
+                                        textAlign: TextAlign.start),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Wrap(
+                                  alignment: WrapAlignment.end,
+                                  children: [
+                                    Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.end,
+                                      children: [
+                                        Text(coin.coinTypes!.symbol.toString().toUpperCase() +" "+ coin.balance!.toString(), style: TextStyle(
+                                            color: Colors.black, fontSize: 18),
+                                            textAlign: TextAlign.start),
+                                        Text(rateText, style: TextStyle(
+                                            color: Color(0xFFC9782F), fontSize: 18),
+                                            textAlign: TextAlign.start),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    shrinkWrap: true,
+                    itemCount: 3,
+                  );
+                },
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Card(
